0f8605497d2079ddd5c6b7c3c2aff52aad4a27f7
borjasolerr <borjasolrey@gmail.com>
2019-03-20 16:55:25 +0100

    Created routes for users and posts

--- postFolder/postRoutes.js
+++ postFolder/postRoutes.js
@@ -6,12 +6,73 @@ const routes = express.Router();
 // Initialize the req.body object
 routes.use(express.json());
 
-routes.get('/', async (req, res) => {
+routes.get('/', async (req, res, next) => {
   try {
     const posts = await Posts.get();
     res.status(200).json(posts);
   } catch {
-    res.status(500).json({ error: 'The posts information could not be retrieved.' });
+    next({ message: 'Posts could not be retrieved.' });
+  }
+});
+
+routes.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const post = await Posts.getById(id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ error: 'Post with this ID does not exist' });
+    }
+  } catch {
+    next('Server Error. Post information could not be retrieved.');
+  }
+});
+
+routes.post('/', async (req, res, next) => {
+  if (req.body.text && req.body.user_id) {
+    try {
+      const posts = await Posts.insert(req.body);
+      res.status(200).json(posts);
+    } catch {
+      next('Server Error. Could not create new post.');
+    }
+  } else {
+    res.status(400).json({ error: 'Please provide text and user_id for the post.' });
+  }
+});
+
+routes.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletedPost = await Posts.remove(id);
+    if (deletedPost > 0) {
+      res.status(200).json({ message: `Post with ID ${id} was deleted.` });
+    } else {
+      res.status(404).json({ error: `Post with ID ${id} does not exist` });
+    }
+  } catch {
+    next('Server Error. Post information could not be retrieved.');
+  }
+});
+
+routes.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  if (req.body.text) {
+    try {
+      const postToUpdate = await Posts.getById(id);
+      // allow the user to update only text of the post
+      const updatedPost = await Posts.update(id, { ...postToUpdate, text: req.body.text });
+      if (updatedPost > 0) {
+        res.status(200).json({ message: `Post with ID ${id} was edited.` });
+      } else {
+        res.status(404).json({ error: `Post with ID ${id} does not exist` });
+      }
+    } catch {
+      next('Server Error. Post information could not be retrieved.');
+    }
+  } else {
+    res.status(400).json({ error: 'Please provide text for the post.' });
   }
 });
 
--- userFolder/userRoutes.js
+++ userFolder/userRoutes.js
@@ -6,12 +6,72 @@ const routes = express.Router();
 // Initialize the req.body object
 routes.use(express.json());
 
-routes.get('/', async (req, res) => {
+routes.get('/', async (req, res, next) => {
   try {
     const users = await Users.get();
     res.status(200).json(users);
   } catch {
-    res.status(500).json({ error: 'The users information could not be retrieved.' });
+    next('Users information could not be retrieved.');
+  }
+});
+
+routes.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await Users.getById(id);
+    const userPosts = await Users.getUserPosts(id);
+    if (user) {
+      res.status(200).json(userPosts);
+    } else {
+      res.status(404).json({ error: 'User with this ID does not exist' });
+    }
+  } catch {
+    next('Server Error. User information could not be retrieved.');
+  }
+});
+
+routes.post('/', async (req, res, next) => {
+  if (req.body.name) {
+    try {
+      const users = await Users.insert(req.body);
+      res.status(200).json(users);
+    } catch {
+      next('Server Error. Could not create new user.');
+    }
+  } else {
+    res.status(400).json({ error: 'Please provide name for the user.' });
+  }
+});
+
+routes.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await Users.remove(id);
+    if (deletedUser > 0) {
+      res.status(200).json({ message: `User with ID ${id} was deleted.` });
+    } else {
+      res.status(404).json({ error: `User with ID ${id} does not exist` });
+    }
+  } catch {
+    next('Server Error. User information could not be retrieved.');
+  }
+});
+
+routes.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  if (req.body.name) {
+    try {
+      const updatedUser = await Users.update(id, req.body);
+      if (updatedUser > 0) {
+        res.status(200).json({ message: `User with ID ${id} was edited.` });
+      } else {
+        res.status(404).json({ error: `User with ID ${id} does not exist` });
+      }
+    } catch {
+      next('Server Error. User information could not be retrieved.');
+    }
+  } else {
+    res.status(400).json({ error: 'Please provide name for the user.' });
   }
 });
 
